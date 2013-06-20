80cd9cc268d0fb536924ea1b3daf756b75e1cd96
Dan Tello <dan.tello@gmail.com>
2013-06-19 20:14:27 -0400

    Version 1.0
    - Added tests
    - Refactored code

--- jquery.maxLines.js
+++ jquery.maxLines.js
@@ -1,5 +1,5 @@
 /*
-	jquery.maxLines plugin v0.1
+	jquery.maxLines plugin v1.0
 	---
 	https://github.com/greypants/jQuery.maxLines
 */
@@ -7,20 +7,22 @@
 ;(function($){
 	var pluginName = 'maxLines';
 
-	var Plugin = function(el, maxLines, paddingCompensation){
+	var Plugin = function(el, maxLines, paddingCompensation) {
 		this.el = el;
 		this.$el = $(el);
-		this.paddingCompensation = paddingCompensation === false ? paddingCompensation : true;
-		this.maxLines = Number(maxLines);
-		(this.maxLines === 0) ? this.removeMax() : this.setMax();
 	};
 
 	Plugin.prototype = {
+		init: function(maxLines, paddingCompensation) {
+			this.paddingCompensation = paddingCompensation === false ? paddingCompensation : true;
+			this.maxLines = Number(maxLines);
+			(this.maxLines === 0) ? this.removeMax() : this.setMax();
+
+			return this;
+		},
+
 		setMax: function(){
-			var lineHeight = this.getLineHeight();
-			var paddingBottom = this.getPixelSpacingValue('padding-bottom');
-			var marginBottom = this.getPixelSpacingValue('margin-bottom');
-			var adjustedMarginBottom = marginBottom + paddingBottom + 'px';
+			var lineHeight = this.measureLineHeight();
 			var maxHeight = (this.maxLines * lineHeight) + 'px';
 
 			this.$el.css({
@@ -29,13 +31,22 @@
 				'padding-bottom': '0'
 			});
 
+			if(this.paddingCompensation) {
+				this.compensateForPadding();
+			}
+		},
+
+		compensateForPadding: function() {
 			// Transform padding-bottom into margin-bottom
 			// to keep element spacing consistent.
-			if(this.paddingCompensation) {
-				this.$el.css({
-					'margin-bottom': adjustedMarginBottom
-				});
-			}
+
+			var paddingBottom = this.getPixelValue(this.$el, 'padding-bottom');
+			var marginBottom = this.getPixelValue(this.$el, 'margin-bottom');
+			var adjustedMarginBottom = marginBottom + paddingBottom + 'px';
+
+			this.$el.css({
+				'margin-bottom': adjustedMarginBottom
+			});
 		},
 
 		removeMax: function() {
@@ -47,35 +58,12 @@
 			});
 		},
 
-		getPixelSpacingValue: function(property) {
-			var value = this.$el.css(property);
-			var pixelValue;
-
-			if(value.indexOf('px')) {
-					pixelValue = value.split('px')[0];
-
-			} else if(value.indexOf('em')) {
-					var ems = value.split('em')[0];
-					pixelValue = ems * baseFontSize;
-			}
-
-			return Number(pixelValue);
+		getPixelValue: function($el, property) {
+			return Number($el.css(property).split('px')[0]);
 		},
 
-		getPixelFontSize: function(fontSize) {
-			if(fontSize.indexOf('px') > 0) {
-				return fontSize.split('px')[0];
-
-			} else if(fontSize.indexOf('em') > 0) {
-				return (fontSize.split('em')[0] * baseFontSize) + 0.5 | 0;
-
-			} else if(fontSize.indexOf('rem') > 0) {
-				return (fontSize.split('rem')[0] * baseFontSize)  + 0.5 | 0;
-			}
-		},
-
-		getLineHeight: function() {
-			var $temp = $('<span style="margin: 0; padding: 0;">jT</span>').appendTo(this.$el);
+		measureLineHeight: function() {
+			var $temp = $('<div style="margin: 0; padding: 0; display: block;">jT</div>').appendTo(this.$el);
 			var lineHeight = $temp.height();
 			$temp.remove();
 			return lineHeight;
@@ -84,8 +72,11 @@
 
 	$.fn[pluginName] = function ( maxLines, paddingCompensation) {
 		return this.each(function () {
-			if (!$.data(this, 'plugin_' + pluginName)) {
-				$.data(this, 'plugin_' + pluginName, new Plugin( this, maxLines, paddingCompensation));
+			var plugin = $.data(this, 'plugin_' + pluginName);
+			if (!plugin) {
+				$.data(this, 'plugin_' + pluginName, new Plugin(this).init(maxLines, paddingCompensation));
+			} else {
+				plugin.init(maxLines, paddingCompensation);
 			}
 		});
 	};
---
+++ test/maxLines-test.js
@@ -0,0 +1,69 @@
+describe("jQuery.maxLines", function() {
+
+	var $el, plugin;
+
+	beforeEach(function() {
+		$el = $('<div>A powerful criminal brain from the planet Arous, Gor, assumes the body of scientist Steve March. Thru March he begins to control the world by threatening destruction to any country challenging his domination. Another brain, Val, works with Marchs future wife Sally to defeat Gor. Val explains that Gor will be vulnerable when he is forced to leave March at intervals to re-energize. Gors vulnerable spot, the Fissure of Orlando, is described in a note left by Sally in Steve’s lab.</div>');
+		$el.appendTo(document.body);
+		$el.maxLines(0);
+		plugin = $el.data('plugin_maxLines');
+	});
+
+	afterEach(function(){
+		$el.remove();
+	});
+
+	it ("Accurately measures the line-height!", function() {
+		$el.css('line-height', '99px');
+		var measuredHeight = plugin.measureLineHeight();
+		measuredHeight.should.equal(99);
+	});
+
+	it ("Can get pixel values as numbers from non-pixel based css properties on objects!", function() {
+		$el.css('margin-bottom', '20em');
+		var parentFontSize = $el.parent().css('font-size').split('px')[0];
+		plugin.getPixelValue($el, 'margin-bottom').should.equal(20 * parentFontSize);
+	});
+
+	it ("Removes previously set values!", function() {
+		$el.css({
+			'max-height': '99px',
+			'margin-bottom': '20em',
+			'padding-bottom': '39%',
+			'overflow': 'scroll'
+		});
+
+		plugin.removeMax();
+
+		$el.css('max-height').should.equal('none');
+		$el.css('margin-bottom').should.equal('0px');
+		$el.css('padding-bottom').should.equal('0px');
+		$el.css('overflow').should.equal('visible');
+	});
+
+	it ("Can be passed a number sets a max-height!", function() {
+		$el.maxLines(3);
+		$el.css('max-height').should.not.equal('none');
+	});
+
+	it ("Can be passed a number-string sets a max-height!", function() {
+		$el.maxLines('3');
+		$el.css('max-height').should.not.equal('none');
+	});
+
+	it ("Can be passed 0 to remove the max-height!", function() {
+		$el.maxLines(3);
+		$el.maxLines(0);
+		$el.css('max-height').should.equal('none');
+	});
+
+	it ("Should default paddingCompenstion to 'true'!", function() {
+		$el.maxLines(99);
+		plugin.paddingCompensation.should.equal(true);
+	});
+
+	it ("Can be passed a second value of 'false' to disable padding compensation!", function() {
+		$el.maxLines(3, false);
+		plugin.paddingCompensation.should.equal(false);
+	});
+});
